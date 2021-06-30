0922cce05a5d690d4af6f7516d4cfae8a63c6f4c
Diney Luiz Antunes <diney.dla@gmail.com>
2021-06-30 20:44:15 -0300

    refactor: 💡 rename variable

--- truck-management/domain/truck_validator.go
+++ truck-management/domain/truck_validator.go
@@ -1,13 +1,11 @@
 package domain
 
-type TruckValidator func(newTruck Truck, existingTruck Truck) error
-
-func IsValidTruck(newTruck Truck, existingTruck Truck) error {
-	if newTruck.LicensePlate == existingTruck.LicensePlate {
+func IsValidTruck(newTruck Truck, possibleExistingTruck Truck) error {
+	if newTruck.LicensePlate == possibleExistingTruck.LicensePlate {
 		return NewConflict("there is already a truck registered with this License Plate")
 	}
 
-	if newTruck.EldID == existingTruck.EldID {
+	if newTruck.EldID == possibleExistingTruck.EldID {
 		return NewConflict("there is already a truck registered with this ELD id")
 	}
 
