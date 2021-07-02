bc83e996ef33222dd9e03f6920c7e6818f6f9b6a
Diney Luiz Antunes <diney.dla@gmail.com>
2021-07-02 13:39:35 -0300

    feat: 🎸 add more fields valitators

--- truck-management/domain/truck_entity.go
+++ truck-management/domain/truck_entity.go
@@ -5,12 +5,12 @@ type Truck struct {
 	LicensePlate string `json:"license_plate" validate:"required,alphanum,min=7,max=7"`
 	EldID        string `json:"eld_id" validate:"required,ascii,max=20"`
 	CarrierID    string `json:"carrier_id" validate:"required,ascii,max=50"`
-	Type         string `json:"type"`
-	Size         int    `json:"size"`
-	Color        string `json:"color"`
-	Make         string `json:"make"`
-	Model        string `json:"model"`
-	Year         int    `json:"year"`
+	Type         string `json:"type" validate:"ascii,max=20"`
+	Size         int    `json:"size" validate:"numeric,min=20,max=50"`
+	Color        string `json:"color" validate:"ascii,max=20"`
+	Make         string `json:"make" validate:"ascii,max=20"`
+	Model        string `json:"model" validate:"ascii,max=20"`
+	Year         int    `json:"year" validate:"numeric,min=1900"`
 	CreatedAt    string `json:"created_at"`
 	UpdatedAt    string `json:"updated_at"`
 }
